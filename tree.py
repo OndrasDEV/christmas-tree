185cbd7cdbf4f433bc85423d2f0059608f0c7db0
Ondras <101731277+OndrasDEV@users.noreply.github.com>
2023-12-11 17:44:14 +0100

    Create tree.py

---
+++ tree.py
@@ -0,0 +1,24 @@
+from colorama import Fore
+
+
+
+tree = f"""
+{Fore.YELLOW}      .
+{Fore.YELLOW}   __/ \__
+{Fore.YELLOW}   \     /
+{Fore.YELLOW}   /{Fore.GREEN}.'{Fore.RED}o{Fore.GREEN}'.{Fore.YELLOW}\{Fore.RESET}
+    {Fore.GREEN}.{Fore.BLUE}o{Fore.GREEN}.'.{Fore.RESET}
+   {Fore.GREEN}.'.'{Fore.YELLOW}o{Fore.GREEN}'.{Fore.RESET}
+  {Fore.CYAN}o{Fore.GREEN}'.{Fore.WHITE}o{Fore.GREEN}.'.{Fore.RED}o.{Fore.RESET}
+ {Fore.GREEN}.'.{Fore.BLUE}o{Fore.GREEN}.'.'.{Fore.LIGHTGREEN_EX}o{Fore.GREEN}.{Fore.RESET}
+{Fore.GREEN}.{Fore.RED}o{Fore.GREEN}.'.{Fore.LIGHTRED_EX}o{Fore.GREEN}.'.{Fore.MAGENTA}o{Fore.GREEN}.'.{Fore.RESET}
+   {Fore.LIGHTBLACK_EX}[_____]{Fore.RESET}
+    {Fore.LIGHTBLACK_EX}\___/{Fore.RESET}
+    
+    
+made by {Fore.RED}o{Fore.GREEN}n{Fore.YELLOW}d{Fore.BLUE}r{Fore.MAGENTA}a{Fore.CYAN}s{Fore.RESET}
+{Fore.RED}M{Fore.WHITE}e{Fore.RED}r{Fore.WHITE}r{Fore.RED}y{Fore.WHITE} {Fore.RED}C{Fore.WHITE}h{Fore.RED}r{Fore.WHITE}i{Fore.RED}s{Fore.WHITE}t{Fore.RED}m{Fore.WHITE}a{Fore.RED}s{Fore.WHITE}!{Fore.RESET}
+"""
+
+
+print(tree)
